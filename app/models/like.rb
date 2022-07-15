98ed833daf0e859bf2110fd5df3a52855b18e305
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-07-15 11:46:48 -0500

    Create Likes Methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post, counter_cache: :likes_counter
+
+  def update_likes_counter
+    post.likes_counter = Like.where(post_id:).size
+  end
 end
