5378c57d8ea415472aa0a3f7a443c044e465db0f
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-07-15 11:58:06 -0500

    Create Validations for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
+  validates :title, precense: true
+  validates :title, length: { maximum: 250 }
+  validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }
+  
   belongs_to :author, class_name: 'User', foreign_key: :author_id, counter_cache: :posts_counter
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
