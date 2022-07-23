c645a44cda2a250898859267b7fcd9b41709f43c
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-07-23 13:03:27 -0500

    Linters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,6 @@ class CommentsController < ApplicationController
 
   def create
     @user = current_user
-    if Comment.create(author_id: @user.id, post_id: params[:post_id], text: params[:text])
-      redirect_to user_posts_path(params[:user_id])
-    end
-  end
+    comment = Comment.new(author_id: @user.id, post_id: params[:post_id], text: params[:text])
+    redirect_to user_posts_path(params[:user_id]) if comment.save
 end
