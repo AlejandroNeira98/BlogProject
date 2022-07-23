95fb439ade90606ff22be192b95bf2843680a314
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-07-23 12:30:28 -0500

    Update controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,10 @@
 class CommentsController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    @user = current_user
+    if Comment.create(author_id: @user.id, post_id: params[:post_id], text: params[:text])
+      redirect_to user_posts_path(params[:user_id])
+    end
+  end
 end
